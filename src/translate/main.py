f8c938c273ce62a5df80e0ae6c66e43413d673bb
muhammed <info@bayram.dev>
2025-07-10 20:38:26 +0200

    Fix typo - embarassing shit

--- src/translate/main.py
+++ src/translate/main.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from src.translate.pre_process import strip_gutenberg_chunks
 
 input_dir = Path("/") / "home" / "anf3fe" / "book_in_dir"
-orig_path = input_dir / "the_idiot.txt"  # englische uebersetzung
+origin_path = input_dir / "the_idiot.txt"  # englische uebersetzung
 
-with open(orig_path, "r") as f:
+with open(origin_path, "r") as f:
     orig = f.read()
 
 
